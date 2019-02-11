55d6fc44b90b05edb10b7fae8ecd47244cc066b8
Austin Ouyang <aouyang1@gmail.com>
2019-02-11 00:21:31 -0800

    cleaned up formatting issues

--- src/components/Motifs.vue
+++ src/components/Motifs.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="motifs">
-    <div v-for="(data, index) in motifOptions" :key='index'>
+    <div v-for="(data, index) in motifOptions" :key="index">
       <highcharts :options="data.chartOptions"></highcharts>
     </div>
   </div>
@@ -12,51 +12,67 @@ export default {
   data() {
     return {
       motifOptions: [
-        {"chartOptions": {
-          chart: {height: "300px"},
-          title: {text: "motif 1"},
-          xAxis: [{labels:{enabled:false}}],
-          yAxis: [{
-            title: "",
-            labels:{enabled:false}
-          }],
-          series: [
-            {
-              showInLegend: false,
-              data: [1, 2, 3]
-            },
-            {
-              showInLegend: false,
-              data: [1.1, 2.1, 3.1]
-            }
-          ]
-        }},
-        {"chartOptions": {
-          chart: {height: "300px"},
-          title: {text: "motif 2"},
-          xAxis: [{labels:{enabled:false}}],
-          yAxis: [{
-            title: "",
-            labels:{enabled:false}
-          }],
-          series: [{
-            showInLegend: false,
-            data: [1, 2, 1]
-          }]
-        }},
-        {"chartOptions": {
-          chart: {height: "300px"},
-          title: {text: "motif 3"},
-          xAxis: [{labels:{enabled:false}}],
-          yAxis: [{
-            title: "",
-            labels:{enabled:false}
-          }],
-          series: [{
-            showInLegend: false,
-            data: [1, 2, 0]
-          }]
-        }}
+        {
+          chartOptions: {
+            chart: { height: "300px" },
+            title: { text: "motif 1" },
+            xAxis: [{ labels: { enabled: false } }],
+            yAxis: [
+              {
+                title: "",
+                labels: { enabled: false }
+              }
+            ],
+            series: [
+              {
+                showInLegend: false,
+                data: [1, 2, 3]
+              },
+              {
+                showInLegend: false,
+                data: [1.1, 2.1, 3.1]
+              }
+            ]
+          }
+        },
+        {
+          chartOptions: {
+            chart: { height: "300px" },
+            title: { text: "motif 2" },
+            xAxis: [{ labels: { enabled: false } }],
+            yAxis: [
+              {
+                title: "",
+                labels: { enabled: false }
+              }
+            ],
+            series: [
+              {
+                showInLegend: false,
+                data: [1, 2, 1]
+              }
+            ]
+          }
+        },
+        {
+          chartOptions: {
+            chart: { height: "300px" },
+            title: { text: "motif 3" },
+            xAxis: [{ labels: { enabled: false } }],
+            yAxis: [
+              {
+                title: "",
+                labels: { enabled: false }
+              }
+            ],
+            series: [
+              {
+                showInLegend: false,
+                data: [1, 2, 0]
+              }
+            ]
+          }
+        }
       ]
     };
   }
--- src/components/TimeSeries.vue
+++ src/components/TimeSeries.vue
@@ -1,28 +1,29 @@
 <template>
   <div>
     <highcharts :options="chartOptions"></highcharts>
-      <b-input-group prepend="m">
-        <b-form-input type="number" placeholder="subsequence length"></b-form-input>
-        <b-input-group-append><b-btn>Calculate</b-btn></b-input-group-append>
-      </b-input-group>
+    <b-input-group prepend="m">
+      <b-form-input type="number" placeholder="subsequence length">
+      </b-form-input>
+      <b-input-group-append><b-btn>Calculate</b-btn></b-input-group-append>
+    </b-input-group>
   </div>
 </template>
 
 <script>
-import ts from "../data/testdata.json"
+import ts from "../data/testdata.json";
 export default {
   name: "TimeSeries",
   data() {
     return {
       chartOptions: {
-        chart: {"zoomType": "x"},
+        chart: { zoomType: "x" },
         tooltip: false,
-        title: {text: "Time Series"},
+        title: { text: "Time Series" },
         plotOptions: {
           series: {
             lineWidth: 1,
             states: {
-              hover: {enabled: false}
+              hover: { enabled: false }
             }
           }
         },
@@ -39,6 +40,4 @@ export default {
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-
-</style>
+<style scoped></style>
