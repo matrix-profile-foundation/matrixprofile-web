0f3de3cb33a79324c0bb3f5824156d9c73ae3cbe
Austin Ouyang <aouyang1@gmail.com>
2019-05-15 22:44:48 -0700

    changed mp stomp concurrency back to 2

--- mpserver/main.go
+++ mpserver/main.go
@@ -14,7 +14,7 @@ import (
 )
 
 var (
-	mpConcurrency    = 4
+	mpConcurrency    = 2
 	maxRedisBlobSize = 10 * 1024 * 1024
 	retentionPeriod  = 5 * 60
 	redisURL         = "localhost:6379" // override with REDIS_URL environment variable
